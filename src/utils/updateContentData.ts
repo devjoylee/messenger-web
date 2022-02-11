e6465755dcd54b5cac0bd3dd54b1696f742291d1
Joy Lee <joylee.dev@gmail.com>
2022-02-12 01:08:05 +0900

    ♻ type 수정, 스크롤바 스타일 추가

--- src/utils/updateContentData.ts
+++ src/utils/updateContentData.ts
@@ -1,13 +1,18 @@
 import { doc, updateDoc, arrayUnion } from 'firebase/firestore';
 import { db } from 'server/firebase';
 import { Content } from 'types';
-import { getDate } from 'utils/getDate';
 
-export const updateContentData = async ({ text, date, userId }: Content) => {
+export const updateContentData = async ({
+  uuid,
+  text,
+  date,
+  userId,
+}: Content) => {
   await updateDoc(doc(db, 'content', 'E1bHxak2ZndSED1tkXdp'), {
     content: arrayUnion({
+      uuid: uuid,
       text: text,
-      date: getDate(date),
+      date: date,
       userId: userId,
     }),
   });
