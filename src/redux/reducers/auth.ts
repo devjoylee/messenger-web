4fa410bb911c3d2300de7c2e0ccf445b6cb932f1
hoonjoo94 <hoonjoo94@naver.com>
2022-02-12 06:08:05 +0900

    ✨ 메시지 답장 기능 구현 완료

--- src/redux/reducers/auth.ts
+++ src/redux/reducers/auth.ts
@@ -1,6 +1,10 @@
-import { GET_USERS, UPDATE_CURRENT_USER } from 'redux/actions/types';
+import {
+  GET_USERS,
+  SET_REPLY_USER,
+  UPDATE_CURRENT_USER,
+} from 'redux/actions/types';
 
-const initialState = { users: [], currentUser: null };
+const initialState = { users: [], currentUser: null, toReply: null };
 
 export default function auth(state = initialState, action: any) {
   switch (action.type) {
@@ -8,6 +12,8 @@ export default function auth(state = initialState, action: any) {
       return { ...state, users: action.payload };
     case UPDATE_CURRENT_USER:
       return { ...state, currentUser: action.payload };
+    case SET_REPLY_USER:
+      return { ...state, toReply: action.payload };
     default:
       return state;
   }
