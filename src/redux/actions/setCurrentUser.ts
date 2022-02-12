77c25a612a427fa113a2c4b9c83a0916496bd8e9
hoonjoo94 <hoonjoo94@naver.com>
2022-02-12 14:31:52 +0900

    ✨ 로그아웃 기능 구현

--- src/redux/actions/setCurrentUser.ts
+++ src/redux/actions/setCurrentUser.ts
@@ -1,7 +1,7 @@
 import { User } from 'types';
 import { UPDATE_CURRENT_USER } from './types';
 
-export const setCurrentUser = (user: User) => ({
+export const setCurrentUser = (user: User | null) => ({
   type: UPDATE_CURRENT_USER,
   payload: user,
 });
